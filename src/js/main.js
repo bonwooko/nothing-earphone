6ed5304fb405830225115d795f2212c012462a5b
bonwooko <ko.bonwoo@gmail.com>
2022-03-22 01:16:23 +0900

    navbar done

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+// ========== Nav menu toggle ==========
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.querySelector('.nav__close');
+const navShadow = document.querySelector('.nav__shadow');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.add('show__menu');
+  navShadow.classList.add('show__menu-shadow');
+});
+
+navClose.addEventListener('click', () => {
+  navMenu.classList.remove('show__menu');
+  navShadow.classList.remove('show__menu-shadow');
+});
