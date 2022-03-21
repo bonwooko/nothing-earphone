1af6e31ca81bb90de09ecefd2946f0f3a225616c
bonwooko <ko.bonwoo@gmail.com>
2022-03-22 04:03:44 +0900

    mobile view - home done

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,16 @@ const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.querySelector('.nav__close');
 const navShadow = document.querySelector('.nav__shadow');
+const homeDot = document.querySelector('.home__dot');
 
 navToggle.addEventListener('click', () => {
   navMenu.classList.add('show__menu');
   navShadow.classList.add('show__menu-shadow');
+  homeDot.style.visibility = 'hidden';
 });
 
 navClose.addEventListener('click', () => {
   navMenu.classList.remove('show__menu');
   navShadow.classList.remove('show__menu-shadow');
+  homeDot.style.visibility = 'visible';
 });
