e8fa893b9ef47699fbfc45375308965e79186570
bonwooko <ko.bonwoo@gmail.com>
2022-03-25 02:32:00 +0900

    mobile view - footer - accordion menu

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,21 @@ navClose.addEventListener('click', () => {
   navMenu.classList.remove('show__menu');
   navShadow.classList.remove('show__menu-shadow');
 });
+
+// ========== Footer Accordion Menu ==========
+const accordionToggles = document.querySelectorAll('.footer__list-title');
+const accordionIcons = document.querySelectorAll('.bx-chevron-down');
+const accordionItems = document.querySelectorAll('.footer__items');
+
+accordionToggles.forEach((list) => {
+  list.addEventListener('click', () => {
+    toggleAccordion(list);
+  });
+});
+
+function toggleAccordion(el) {
+  const icon = el.children[1];
+  const item = el.nextElementSibling;
+  icon.classList.toggle('icon-active');
+  item.classList.toggle('active');
+}
