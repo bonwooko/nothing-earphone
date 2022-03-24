f140b53a83532606ffaef9ffc950d7bb5bc03caf
bonwooko <ko.bonwoo@gmail.com>
2022-03-24 17:24:19 +0900

    mobile view - infinite marquee

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,19 @@
+import gsap from 'gsap';
+
 // ========== Nav menu toggle ==========
 const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.querySelector('.nav__close');
 const navShadow = document.querySelector('.nav__shadow');
-const homeDot = document.querySelector('.home__dot');
 
 navToggle.addEventListener('click', () => {
   navMenu.classList.add('show__menu');
   navShadow.classList.add('show__menu-shadow');
-  homeDot.style.visibility = 'hidden';
 });
 
 navClose.addEventListener('click', () => {
   navMenu.classList.remove('show__menu');
   navShadow.classList.remove('show__menu-shadow');
-  homeDot.style.visibility = 'visible';
 });
+
+// ========== Infinite Text Marquee ==========
