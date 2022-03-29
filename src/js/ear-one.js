639aa4d2fcb37e96c56fde7e68c818afae6764ec
bonwooko <ko.bonwoo@gmail.com>
2022-03-29 20:24:35 +0900

    product page - mobile view - product description swiper

--- src/js/ear-one.js
+++ src/js/ear-one.js
@@ -12,20 +12,40 @@ const whiteSwiper = new Swiper('.white__earphone', {
 });
 
 // ===== show white or black earphone =====
+const productButtons = document.querySelectorAll('.color__button');
 const blackButton = document.getElementById('button-black');
 const whiteButton = document.getElementById('button-white');
 const productContainer = document.querySelector('.product__container');
 const blackEarphone = document.querySelector('.black__earphone');
 const whiteEarphone = document.querySelector('.white__earphone');
+const productTextColor = document.querySelector('.product__text-wrap');
 
 blackButton.addEventListener('click', () => {
   productContainer.style.backgroundColor = 'var(--color-white)';
   blackEarphone.classList.remove('hide__swiper');
   whiteEarphone.classList.add('hide__swiper');
+  productTextColor.style.color = 'var(--color-black)';
+  productButtons.forEach((button) => {
+    button.style.color = 'var(--color-white)';
+    button.style.backgroundColor = 'var(--color-black)';
+  });
 });
 
 whiteButton.addEventListener('click', () => {
   productContainer.style.backgroundColor = 'var(--color-black)';
   blackEarphone.classList.add('hide__swiper');
   whiteEarphone.classList.remove('hide__swiper');
+  productTextColor.style.color = 'var(--color-white)';
+  productButtons.forEach((button) => {
+    button.style.color = 'var(--color-black)';
+    button.style.backgroundColor = 'var(--color-white)';
+  });
+});
+
+// ===== product image swiper =====
+let detailSwiper = new Swiper('.description__detail-swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 });
