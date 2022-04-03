774861c3608434537c94c714d56b0b7b011bddf7
bonwooko <ko.bonwoo@gmail.com>
2022-04-04 01:15:38 +0900

    about page - mobile view - our story, investors, sustainability

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -5,3 +5,19 @@ const asideList = document.querySelector('.about__aside-list');
 asideTitle.addEventListener('click', () => {
   asideList.classList.toggle('show__aside-list');
 });
+
+// ===== OUR STORY SWIPER =====
+const ourStorySwiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
