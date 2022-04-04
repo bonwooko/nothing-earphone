57e60e7a9bd565416abd78d3103f9bcdff1ddd29
bonwooko <ko.bonwoo@gmail.com>
2022-04-04 18:08:45 +0900

    about page - mobile view - aside sticky, smooth scroll to specific margin of each section

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -6,6 +6,15 @@ asideTitle.addEventListener('click', () => {
   asideList.classList.toggle('show__aside-list');
 });
 
+// ===== HIDE ASIDE LIST AFTER CLICKING THE ASIDE LINK =====
+const asideLinks = document.querySelectorAll('.about__aside-link');
+
+asideLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    asideList.classList.remove('show__aside-list');
+  });
+});
+
 // ===== OUR STORY SWIPER =====
 const ourStorySwiper = new Swiper('.mySwiper', {
   slidesPerView: 1,
